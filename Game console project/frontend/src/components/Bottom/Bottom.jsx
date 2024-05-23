cba1e87a3f33910e790e2b276c381cb8e53b9dc4
william <williams_wandji@yahoo.fr>
2024-05-23 17:23:12 +0200

    end bottom

--- Game console project/frontend/src/components/Bottom/Bottom.jsx
+++ Game console project/frontend/src/components/Bottom/Bottom.jsx
@@ -9,16 +9,60 @@ export default function Footbar() {
           <h1>Subscribe newlestter</h1>
         </div>
         <div className="input-subscribe">
-          <form>
-            <input type="text" name="subscribe" placeholder="Email adress" />
-            <button>Submit</button>
-          </form>
+          <input type="text" name="subscribe" placeholder="Email adress" />
+          <img src="send-2.svg" />
         </div>
         <div className="call">
           <div className="logo-call">
-            <img src= 'headphone.svg' />
+            <img src="headphone.svg" />
           </div>
-          <div>Call us 24 / 7 : <br/> 0747835976</div>
+          <div>
+            <p>
+              Call us 24 / 7 : <br /> 0747835976
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="foot-bottom">
+        <div className="left-bottom">
+          <div className="logo-site">
+            <img src="Union.svg" />
+            <p>Game center</p>
+          </div>
+          <div className="adress">
+            <p>
+              64 st james boulevard <br />
+              hoswick, ze2 7zj
+            </p>
+          </div>
+        </div>
+        <div className="mid-left-bottom">
+          <p>Find product</p>
+          <ul>
+            <li>Playstation</li>
+            <li>Xbox One</li>
+            <li>Nintendo DS</li>
+          </ul>
+        </div>
+        <div className="mid-right-bottom">
+          <p>Get help</p>
+          <ul>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Return policy</li>
+            <li>Privacy policy</li>
+            <li>Payment policy</li>
+          </ul>
+        </div>
+        <div className="right-bottom">
+          <p>About us</p>
+          <ul>
+            <li>News</li>
+            <li>Service</li>
+            <li>Our policy</li>
+            <li>Customer care</li>
+            <li>Faq's</li>
+          </ul>
         </div>
       </div>
     </div>
