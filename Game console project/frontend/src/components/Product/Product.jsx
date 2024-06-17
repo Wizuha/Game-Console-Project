eeffa628d6a20df92a6f75f824ba6907d3e543de
william <williams_wandji@yahoo.fr>
2024-06-17 18:02:23 +0200

    add ccbling product for rate

--- Game console project/frontend/src/components/Product/Product.jsx
+++ Game console project/frontend/src/components/Product/Product.jsx
@@ -7,7 +7,8 @@ export default function Product() {
   const [products, setProducts] = useState([]);
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
-  
+  const [rate, setRate] = useState(0);
+
   useEffect(() => {
     fetch("http://localhost:8000/")
       .then((response) => response.json())
@@ -15,14 +16,21 @@ export default function Product() {
       .catch((error) => console.error("Error fetching product", error));
   }, []);
 
-  const ciblingProduct = (id) => {
-    
-  }
+  const ciblingProduct = (id, newRate) => {
+    const productsUpdated = products.map((product, index) => {
+      if (product.id_console == id) {
+        // product.rate = rate ;
+        return { ...product, rate: newRate };
+      }
+      return product;
+    });
+    setProducts(productsUpdated);
+  };
 
   return (
     <div className="console-product">
       {products.map((product, index) => (
-        <div key={index} className="product">
+        <div key={index} className="product" id={product.id_console}>
           <div className="product-item">
             <div className="image">
               <img src="logo192.png" alt="Product" />
@@ -33,21 +41,29 @@ export default function Product() {
               <p className="price">{product.price}€</p>
               <div className="rate">
                 {[...Array(5)].map((star, index) => {
-                  const currentRating = index + 1;
+                  const currentRating = product.rate;
                   return (
-                    <label>
+                    <label key={index}>
                       <input
                         type="radio"
                         name="rating"
                         value={currentRating}
-                        onClick={() => setRating(currentRating)}
+                        onClick={() =>
+                          ciblingProduct(product.id_console, index)
+                        }
                       />
-                      <FaStar 
-                        className="star" 
-                        size={20} 
-                        color = {currentRating <= (hover || rating) ? "ACACAC" : "e4e5e9" }
-                        onMouseEnter = {() => setHover(currentRating)}
-                        onMouseLeave = {() => setHover(null)}
+                      <FaStar
+                        id={index}
+                        className="star"
+                        size={20}
+                        // color={
+                        //   currentRating <= (hover || rating)
+                        //     ? "ACACAC"
+                        //     : "e4e5e9"
+                        // }
+                        color={index <= currentRating ? "ACACAC" : "e4e5e9"}
+                        onMouseEnter={() => setHover(currentRating)}
+                        onMouseLeave={() => setHover(null)}
                       />
                     </label>
                   );
