b28c8f972050ed98db80035395ad7558411d7bd9
william <williams_wandji@yahoo.fr>
2024-07-01 09:01:54 +0200

    div placement image productdetails page

--- Game console project/frontend/src/pages/InfoProduct/ProductDetails.jsx
+++ Game console project/frontend/src/pages/InfoProduct/ProductDetails.jsx
@@ -1,25 +1,6 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-
-// export default function ProductDetails() {
-//   const { id_console } = useParams();
-//   const [product, setProduct] = useState(null);
-//   useEffect (() => {
-//     fetch(`http://localhost:8000/product/${id_console}`)
-//       .then((response) => response.json())
-//       .then((data) => setProduct(data))
-//       .catch((error) => console.error("Erreur lors du fetch des données :",error.message))
-//   },[])
-
-//   return (
-//     <div>
-//       <p>Nom : {product.name}</p>
-//     </div>
-//   );
-// } 
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import './ProductDetails.css'
 
 export default function ProductDetails() {
   const { id_console } = useParams();
@@ -52,22 +33,25 @@ export default function ProductDetails() {
   if (loading) {
     return <p>Loading...</p>;
   }
-
   if (error) {
     return <p>Erreur : {error}</p>;
   }
-
   if (!product) {
     return <p>Aucun produit trouvé.</p>;
   }
 
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <p>Nom : {product.name}</p>
-      <img src={product.image} alt={product.name} />
-      <p>Description : {product.description}</p>
-      <p>Prix : ${product.price}</p>
+    <div className="product-details">
+      <div className="images">
+        <div className="image-head"></div>
+        <div className="image-bottom">
+          <div className="first-picture"></div>
+          <div className="second-picture"></div>
+        </div>
+      </div>
+      <div className="information-achat">
+        
+      </div>
     </div>
   );
 }
