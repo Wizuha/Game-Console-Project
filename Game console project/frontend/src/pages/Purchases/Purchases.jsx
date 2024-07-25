722f4e59a7aeb5229178ee966bd90fa231f7b95f
Wizuha <williams.wandji@gmail.com>
2024-07-25 19:45:36 +0200

    add proced checkout

--- Game console project/frontend/src/pages/Purchases/Purchases.jsx
+++ Game console project/frontend/src/pages/Purchases/Purchases.jsx
@@ -19,9 +19,40 @@ export default function Purchases() {
             </tr>
           </thead>
           <tbody>
-              {purchases.map((purchase) => (
-                <TabPurchase purchase={purchase} key={purchase.id_console} />                
-              ))}
+            {purchases.map((purchase) => (
+              <TabPurchase purchase={purchase} key={purchase.id_console} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="card-total">
+        <table className="single-column-table">
+          <thead>
+            <tr>
+              <th>Nom du Produit</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="single-column">
+                <div className="subtotal-card-total">
+                  <h2>Subtotal</h2>
+                  <p>$200</p>
+                </div>
+                <div className="line"></div>
+                <div className="coupon">
+                  <input type="text" name="coupon" placeholder="Enter coupon" />
+                  <button>Apply</button>
+                </div>
+                <div className="line"></div>
+                <div className="country">
+                  <input type="text" name="country" placeholder="Country" />
+                </div>
+                <div className="proced-checkout">
+                  <button>Proceed checkout</button>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
